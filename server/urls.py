5d4d5154e85b7c6100f3ab37457312fe62fcaabc
Rajvi Zala <rajvisinhazala@gmail.com>
2023-10-16 21:15:23 +0530

    Media Url Added

--- server/urls.py
+++ server/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('library/', include('library.urls')),
@@ -24,4 +25,5 @@ urlpatterns = [
     path('ai/', include('ai.urls')),
     path('authentication/', include('authentication.urls')),
     
-]
+] 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
